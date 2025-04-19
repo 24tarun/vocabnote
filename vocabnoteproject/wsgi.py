e529c85501df32dc5a2a27f07e9fd697247f9edb
tarunhariharan <tarun2k01@gmail.com>
2025-04-19 05:27:27 +0200

    wsgi case fixes

--- vocabnoteproject/wsgi.py
+++ vocabnoteproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VocabNoteProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vocabnoteproject.settings')
 
 application = get_wsgi_application()
