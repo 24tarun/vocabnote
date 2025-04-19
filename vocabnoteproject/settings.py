e624da26a7c131ca7b8ecd087bdaec110794efe8
tarunhariharan <tarun2k01@gmail.com>
2025-04-19 05:24:10 +0200

    w

--- vocabnoteproject/settings.py
+++ vocabnoteproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-insecure-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vocabnote.onrender.com']
 
 
 # Application definition
