fe7adac56ea6b8b58ae8880b6ce4185ba46c7f45
Tarun Hariharan <thariharan@zentrale.bsa-ba-akademie.de>
2025-08-28 13:43:36 +0200

    i effed up

---
+++ baseapp/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include, re_path
+from django.shortcuts import redirect
+from django.conf.urls import handler404
+
+# Custom handler for 404 errors
+def redirect_to_login(request, exception=None):
+    return redirect('login')
+
+handler404 = 'baseapp.urls.redirect_to_login'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",include("firstapp.urls")),
+    path('accounts/', include('accounts.urls')),
+    re_path(r'^.*$', lambda request: redirect('login')),
+]
